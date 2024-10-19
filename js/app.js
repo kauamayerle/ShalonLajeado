a40e1c8f5df2e0da62129f0e42bf70a9dc7d0f81
Kauã Mayerle <139514362+kauamayerle@users.noreply.github.com>
2024-10-18 21:41:29 -0300

    sim

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
 const navr = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav-list a');
 
 hamburger.addEventListener('click', () => navr.classList.toggle('active'));
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => navr.classList.remove('active'));
+});
